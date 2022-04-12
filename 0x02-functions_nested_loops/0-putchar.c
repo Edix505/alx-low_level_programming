c6d961775063c17d35c7e16a16512c66950c83ce
Edix505 <Edix505@gmail.com>
2022-04-12 11:51:39 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {	
-	
+	{
 		_putchar('_');
                 _putchar('p');
                 _putchar('u');
@@ -17,6 +17,6 @@ int main(void)
                 _putchar('r');
                 _putchar('\n');
 
-	
+	}
 	   return (0);
 }
