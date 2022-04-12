ea2966374c5cc508ecbe8864ff85a2e1c81543a9
Edix505 <Edix505@gmail.com>
2022-04-12 10:21:47 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
-#include "holberton.h"
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 0
  */
-int _putchar(char c)
+int main (void)
 {
-	return (write(1, &c, 1));
+	{	
+		printf (_putchar)
+	}
+	return (0);
 }
