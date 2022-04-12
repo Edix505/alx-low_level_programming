1b765bea161de825ab79fd52502bf33bd5ff760a
Edix505 <Edix505@gmail.com>
2022-04-12 10:57:08 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * print_alphabet - prints the english alphabet from a-z.
- * Return: Nothing.
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main (void)
 {
 	char c;
 
