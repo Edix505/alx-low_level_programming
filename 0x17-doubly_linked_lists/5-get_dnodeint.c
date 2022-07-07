fbfeb4323140f938beb4babc7416f6c4a84b6a3c
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-07 21:52:46 +0200

    a

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Get the nth node of the list
+ * @head: Head of the list
+ * @index: The node to locate
+ *
+ * Return: Node in the current index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *a = head;
+	unsigned int n = 0;
+
+	while (a != NULL)
+	{
+		if (n == index)
+			return (a);
+		n++;
+		a = a->next;
+	}
+
+	return (NULL);
+}
