3192d88ba00053cc96e3806cd7c36f1d313a24df
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-07-07 22:02:25 +0200

    6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sum the data of dlistint_t list.
+ * @head: Head of the linked list
+ *
+ * Return: sum of all the data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *a = head;
+	int agg = 0;
+
+	while (a == NULL)
+		return (agg);
+
+	while (a != NULL)
+	{
+		ans += a->n;
+		a = a->next;
+	}
+	return (agg);
+}
