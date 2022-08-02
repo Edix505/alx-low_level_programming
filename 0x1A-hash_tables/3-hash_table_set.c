d2faad3955bfaf269fe521010db7bdf48d4cfead
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-08-02 19:19:46 +0200

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,40 +1,41 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- *hash_table_set - add an elt to table
- *@ht: pointer hash
- *@key: pointer key
- *@value: value to add
- *Return: 1 if is success or 0
+ * hash_table_set - add an elt to table
+ * @ht: hash table
+ * @key: the key (can't be empty string)
+ * @value: value associated with the key (it can be empty string)
+ * Return: 1 on success 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long index, size;
-hash_node_t *nn;
+	int idx;
+	hash_node_t *n;
 
-if (htt  == NULL || key == NULL || value == NULL)
-return (0);
+	if (key == NULL || strlen(key) == 0 || ht == NULL)
+		return (0);
 
-size = htt->size;
-index = key_index((const unsigned char *)key, size);
+	idx = key_index((const unsigned char *) key, ht->size);
 
-if (htt->array[index] != NULL && strcmp(htt->array[index]->key, key) == 0)
-{
-htt->array[index]->value = strdup(value);
-return (1);
-}
+	n = ht->array[idx];
+	while (n != NULL)
+	{
+		if (strcmp(n->key, key) == 0)
+		{
+			free(n->value);
+			n->value = strdup(value);
+			return (1);
+		}
+		n = n->next;
+	}
 
-nn = malloc(sizeof(hash_node_t));
-
-if (nn == NULL)
-return (0);
-
-nn->key = strdup(key);
-nn->value = strdup(value);
-nn->next = htt->array[index];
-htt->array[index] = nn;
-return (1);
+	n = malloc(sizeof(hash_node_t));
+	if (n == NULL)
+		return (0);
+	n->key = strdup(key);
+	n->value = strdup(value);
+	n->next = ht->array[idx];
+	ht->array[idx] = n;
+	return (1);
 }
